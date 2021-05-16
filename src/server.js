748f6a1df94f7516896e9e80c2b3b5e86300a805
alba054 <prakasamas19h@student.unhas.ac.id>
2021-05-16 16:05:20 +0800

    add environment variable

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ const Hapi = require('@hapi/hapi');
 
 const routes = require('./routes');
 
-// const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV;
 
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5000,
-    // host: 'localhost',
+    host: environment === 'development' ? 'localhost' : '',
     routes: {
       cors: {
         origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],
