0154449428fd2167df7295308f5a3ccd4ef839b2
alba054 <prakasamas19h@student.unhas.ac.id>
2021-05-16 15:50:03 +0800

    fix host bugs again

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const environment = process.env.NODE_ENV;
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: environment !== 'production' ? 'localhost' : 'secret-bastion-35339.herokuapp.com',
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000'],
